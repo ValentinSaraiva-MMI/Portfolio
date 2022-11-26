3c65a9153ede13a06cadc2c3a5a891876d775d90
valentinsa <valentinsa0701@gmail.com>
2022-11-26 17:27:18 -0500

    page 404

--- src/views/Page404View.vue
+++ src/views/Page404View.vue
@@ -2,28 +2,24 @@
   <section>
     <div class="bg-black text-white">
       <div class="flex h-screen">
-        <img src="/images/4044.png" alt="Image de la page 404" />
+        <img src="public\images\4044.png" alt="Image de la page 404" />
       </div>
 
       <div class="-mt-20 flex justify-center">
         <RouterLink to="/">
-          <boutonstroke2>
-            <P>Retour à l'acceuil</P>
-          </boutonstroke2>
+          <button
+            class="h-12 border-2 border-black px-12 text-base text-black transition-[color,box-shadow] duration-[400ms,450ms] hover:border-noir hover:text-noir hover:shadow-[inset_14rem_0_0_0] hover:shadow-white lg:text-lg"
+          >
+            Accueil <i class="fa-regular fa-arrow-down-to-bracket text-white"></i>
+          </button>
         </RouterLink>
       </div>
     </div>
   </section>
 </template>
 
-
 <script>
-/*
-import boutonstroke from '../components/BoutonstrokeView.vue';
-import boutonstroke2 from '../components/BoutonstrokeblackView.vue' */
 export default {
   name: "Page404View ",
-  components: {},
 };
 </script>
-
