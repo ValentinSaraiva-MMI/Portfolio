97204a0b894e5a261104d27a25dcdf30a5e2b8be
valentinsa <valentinsa0701@gmail.com>
2022-11-27 18:36:45 -0500

    ok i pull up

--- src/views/ProjetWebjamView.vue
+++ src/views/ProjetWebjamView.vue
@@ -35,7 +35,7 @@
       </div>
     </div>
 
-    <img src="public/images/mockup/webjam.webp" class="mx-auto mt-20 w-[55%]" alt="Webjam 2022 " />
+    <img src="/public/images/mockup/webjam.webp" class="mx-auto mt-20 w-[55%]" alt="Webjam 2022 " />
 
     <h2 class="ml-[6vw] mt-[5vh] font-montserrat text-3xl font-medium text-white md:text-3xl lg:text-4xl xl:text-5xl">À propos</h2>
     <div class="mt-6 gap-10 md:columns-2 lg:gap-16 xl:gap-20">
